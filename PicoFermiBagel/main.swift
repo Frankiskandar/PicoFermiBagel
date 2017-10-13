5edd1714a450600d5972f4901f9b3f9fe12f43c9
Fnu Frangky <frank@FranksMBP.local>
2017-10-13 15:39:37 -0400

    Initial Commit

---
+++ PicoFermiBagel/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  PicoFermiBagel
+//
+//  Created by Fnu Frangky on 10/13/17.
+//  Copyright © 2017 Fnu Frangky. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
